63c7b5d5a2b479a9767266a273e61f32ae358159
Bahis27 <dude27@yandex.ru>
2019-06-19 18:42:22 +0800

    1 Add web, servlet, logging

--- src/main/java/ru/restaurant/voting/web/VotingServlet.java
+++ src/main/java/ru/restaurant/voting/web/VotingServlet.java
@@ -1,13 +1,21 @@
 package ru.restaurant.voting.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class VotingServlet extends HttpServlet {
+    private static final Logger log = getLogger(VotingServlet.class);
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to voting");
+
         //request.getRequestDispatcher("/voting.jsp").forward(request, response);
         response.sendRedirect("voting.jsp");
     }
