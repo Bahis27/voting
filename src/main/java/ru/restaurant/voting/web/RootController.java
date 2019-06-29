6f2bc088f1e5c1560b3f9657895adb7ae82c5779
Bahis27 <dude27@yandex.ru>
2019-06-29 08:39:58 +0800

    6.3 remove servlets, add Spring MVC

---
+++ src/main/java/ru/restaurant/voting/web/RootController.java
@@ -0,0 +1,24 @@
+package ru.restaurant.voting.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.restaurant.voting.service.user.UserService;
+
+@Controller
+public class RootController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        model.addAttribute("users", service.getAll());
+        return "users";
+    }
+}
